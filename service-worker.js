48c9ad4a9c90c7a21dc5cbff4a73d4e4b2f9dcef
Leonid Yakovlev <leonid@cube.dev>
2021-04-21 12:01:14 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.542e20c2e1d73787670f474d8271dc99.js"
+  "./precache-manifest.492c83fb737f4bbcfa9ebaa33b165c81.js"
 );
 
 self.addEventListener('message', (event) => {
